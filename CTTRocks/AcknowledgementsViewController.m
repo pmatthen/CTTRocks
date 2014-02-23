a88a70ade434b3af74ef327d36e468dd6c539a7f
Stephen Compton <sccompton@me.com>
2014-02-23 14:42:41 -0600

    Added acknowledgemnts view controller and improved help and coach mark function.

---
+++ CTTRocks/AcknowledgementsViewController.m
@@ -0,0 +1,38 @@
+//
+//  AcknowledgementsViewController.m
+//  CTTRocks
+//
+//  Created by Stephen Compton on 2/23/14.
+//  Copyright (c) 2014 Josef Hilbert. All rights reserved.
+//
+
+#import "AcknowledgementsViewController.h"
+
+@interface AcknowledgementsViewController ()
+
+@end
+
+@implementation AcknowledgementsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
