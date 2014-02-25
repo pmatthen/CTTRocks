a6a400ff81bef81a076d1562f53b60fc5150d697
Stephen Compton <sccompton@me.com>
2014-02-25 15:32:36 -0600

    added credits and background image

--- CTTRocks/AcknowledgementsViewController.m
+++ CTTRocks/AcknowledgementsViewController.m
@@ -10,6 +10,11 @@
 
 @interface AcknowledgementsViewController ()
 
+{
+    
+    __weak IBOutlet UIImageView *creditsImageView;
+}
+
 @end
 
 @implementation AcknowledgementsViewController
@@ -26,8 +31,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
+    
+    UIImage * creditsImage = [UIImage imageNamed: @"TribNightView.jpeg"];
+    creditsImageView.image = creditsImage;
+    
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.070 green:0.350 blue:0.60 alpha:1.0]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           shadow, NSShadowAttributeName,
+                                                           [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];
+    self.title = @"Credits";}
 
 -(void)viewWillAppear:(BOOL)animated
 {
